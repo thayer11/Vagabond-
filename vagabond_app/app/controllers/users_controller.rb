27c5d4149f402634ad7b47249833ea977051a2e5
James Scovern <jamie.scovern@gmail.com>
2016-08-03 13:37:00 -0600

    auto login user when account created

--- vagabond_app/app/controllers/users_controller.rb
+++ vagabond_app/app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user=User.find_by_id(params[:id])
+    render :show
   end
 
   # GET /users/new
@@ -27,15 +29,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
+    # respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        login(@user)
+        redirect_to "/users/#{@user.id}"
+        # format.html { redirect_to @user, notice: 'User was successfully created.' }
+        # format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    end
+    # end
   end
 
   # PATCH/PUT /users/1

bc33ac69c63a501328322eb67588a4fa2ac3c717
hockey2249 <kgibbons14@gmail.com>
2016-08-04 14:46:23 -0600

    style

--- vagabond_app/db/seeds.rb
+++ vagabond_app/db/seeds.rb
@@ -1,5 +1,5 @@
 City.destroy_all
-City1= City.create city_name: "Denver", description: "Dry", img_url: "http://cdn.pcwallart.com/images/new-york-city-skyline-2014-wallpaper-1.jpg"
+City1= City.create city_name: "Denver", description: "Dry", img_url: "http://cdn.wallpapersafari.com/12/37/RDWaLI.jpg"
 Post1= Post.create title: 'Good Chinese Food', content: 'Sweet and Sour Chicken..Great!', post_date: '10/20/1982', user_id: 5, city_id: 1
 Post2= Post.create title: "Bed Bugs", content: "There were bed bugs in the train station", post_date: "12/25/2000", user_id: 5, city_id: 1
 Post3= Post.create title: "Nice Weather", content: "Rain makes me happy.", post_date: "01/11/2015", user_id: 5, city_id: 1
